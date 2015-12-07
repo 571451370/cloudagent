4dfc7abcfff52d757302f2f2df8461c67eb7b179
Vasiliy Tolstov <v.tolstov@selfip.ru>
2015-12-07 11:30:56 +0000

    fix logger
    Signed-off-by: Vasiliy Tolstov <v.tolstov@selfip.ru>

--- qga/logger_http.go
+++ qga/logger_http.go
@@ -1,6 +1,7 @@
 package qga
 
 import (
+	"bytes"
 	"crypto/tls"
 	"fmt"
 	"net"
@@ -8,10 +9,16 @@ import (
 	"time"
 )
 
+const (
+	agentLogger = "http://169.254.169.254/agent/log"
+)
+
+// Logger struct
 type Logger struct {
 	w *http.Client
 }
 
+// NewLogger create new http logger
 func NewLogger(c *http.Client) (*Logger, error) {
 	l := &Logger{}
 	if c == nil {
@@ -27,9 +34,11 @@ func NewLogger(c *http.Client) (*Logger, error) {
 	} else {
 		l.w = c
 	}
+
 	return l, nil
 }
 
+// Close closes logger
 func (l *Logger) Close() error {
 	if l.w == nil {
 		return nil
@@ -37,90 +46,44 @@ func (l *Logger) Close() error {
 	return nil
 }
 
-func (l *Logger) Alert(msg string) error {
-	if l.w == nil {
-		return nil
-	}
-	return nil
-}
-
-func (l *Logger) Alertf(f string, msg string) error {
-	return l.Alert(fmt.Sprintf(f, msg))
-}
-
-func (l *Logger) Crit(msg string) error {
-	if l.w == nil {
-		return nil
-	}
-	return nil
-}
-
-func (l *Logger) Critf(f string, msg string) error {
-	return l.Crit(fmt.Sprintf(f, msg))
-}
-
+// Debug send debug message
 func (l *Logger) Debug(msg string) error {
 	if l.w == nil {
 		return nil
 	}
-	return nil
+	_, err := l.w.Post(agentLogger, "text/plain", bytes.NewBufferString("debug: "+msg))
+	return err
 }
 
+// Debugf send debug formatted message
 func (l *Logger) Debugf(f string, msg string) error {
 	return l.Debug(fmt.Sprintf(f, msg))
 }
 
-func (l *Logger) Emerg(msg string) error {
-	if l.w == nil {
-		return nil
-	}
-	return nil
-}
-
-func (l *Logger) Emergf(f string, msg string) error {
-	return l.Emerg(fmt.Sprintf(f, msg))
-}
-
+// Error send error message
 func (l *Logger) Error(msg string) error {
 	if l.w == nil {
 		return nil
 	}
-	return nil
+	_, err := l.w.Post(agentLogger, "text/plain", bytes.NewBufferString("error: "+msg))
+	return err
 }
 
+// Errorf send error formatted message
 func (l *Logger) Errorf(f string, msg string) error {
 	return l.Error(fmt.Sprintf(f, msg))
 }
 
+// Info send info message
 func (l *Logger) Info(msg string) error {
 	if l.w == nil {
 		return nil
 	}
-	return nil
+	_, err := l.w.Post(agentLogger, "text/plain", bytes.NewBufferString("info: "+msg))
+	return err
 }
 
+// Infof send info formatted message
 func (l *Logger) Infof(f string, msg string) error {
 	return l.Info(fmt.Sprintf(f, msg))
-}
-
-func (l *Logger) Notice(msg string) error {
-	if l.w == nil {
-		return nil
-	}
-	return nil
-}
-
-func (l *Logger) Noticef(f string, msg string) error {
-	return l.Notice(fmt.Sprintf(f, msg))
-}
-
-func (l *Logger) Warn(msg string) error {
-	if l.w == nil {
-		return nil
-	}
-	return nil
-}
-
-func (l *Logger) Warnf(f string, msg string) error {
-	return l.Warn(fmt.Sprintf(f, msg))
 }
